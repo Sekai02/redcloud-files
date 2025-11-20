78f3d4a457d27f17eeb9accd40eea5c5d83a1baf
LoLProM <mauriciosundejimenez@gmail.com>
2025-11-20 16:34:54 -0500

    feat: add command request and response data types for file operations

--- cli/types.py
+++ cli/types.py
@@ -13,3 +13,27 @@ class CommandResponse:
 
     def execute_action(self):
         return self.action()
+    
+class AddFileRequest:
+    def __init__(self, file_list: list[str], tag_list: list[str], files_paths: list[str]):
+        self.file_list = file_list
+        self.tag_list = tag_list
+        self.files_paths = files_paths
+
+class DeleteFilesRequest:
+    def __init__(self, tag_query: list[str]):
+        self.tag_query = tag_query
+
+class ListFilesRequest:
+    def __init__(self, tag_query: list[str]):
+        self.tag_query = tag_query
+
+class AddTagsRequest:
+    def __init__(self, tag_query: list[str], tag_list: list[str]):
+        self.tag_query = tag_query
+        self.tag_list = tag_list
+
+class DeleteTagsRequest:
+    def __init__(self, tag_query: list[str], tag_list: list[str]):
+        self.tag_query = tag_query
+        self.tag_list = tag_list
